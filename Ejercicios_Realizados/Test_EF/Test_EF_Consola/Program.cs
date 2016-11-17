e0d070675e3844e191aed7289f54fd9564a4ee72
mcastelarin <mai_clarke_20@hotmail.com>
2016-11-17 20:53:28 -0300

    arreglado

--- Ejercicios_Realizados/Test_EF/Test_EF_Consola/Program.cs
+++ Ejercicios_Realizados/Test_EF/Test_EF_Consola/Program.cs
@@ -14,17 +14,23 @@ namespace Test_EF_Consola
         {
             Test_EF_Context ctx = new Test_EF_Context();
 
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => ctx.Dispose();
 
             if (ctx.Database.Exists())
                 Console.WriteLine("La base existe");
 
             //ctx.Database.Log()
 
-     
+            Console.ReadLine();
 
             Perfil p = ctx.Perfiles.FirstOrDefault();
+
             Console.WriteLine($" {p.Descripcion} ");
+
             Console.ReadLine();
+
+            ctx.Dispose();
         }
+
     }
 }
